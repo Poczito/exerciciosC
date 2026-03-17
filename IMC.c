253a430fd9ee893fc33bbb34f11e7688aa15c6fd
Celso de Oliveira Júnior <117027747+Poczito@users.noreply.github.com>
2026-03-17 01:11:21 +0000

    Criação e edição do arquivo IMC.c

--- IMC.c
+++ IMC.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+int main(){
+
+    float peso = 105.3;
+    float altura = 1.80;
+    float alturaXAltura = altura * altura;
+    float imc = peso / alturaXAltura;
+
+    printf("O IMC é %.2f", imc);
+
+    return 0;
+}
